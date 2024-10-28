27847bda4dd2475a0d360421c9da07d75c7aafe3
Seung-Yoon Oh <feynubrick@proton.me>
2024-10-28 22:35:51 +0900

    스케쥴을 범위로 찾을 수 있게 변경

--- caldal/domain/schedule/filters.py
+++ caldal/domain/schedule/filters.py
@@ -8,6 +8,12 @@ from caldal.util.schemas import CamelCaseConfig
 
 class ScheduleFilterSchema(FilterSchema):
     date: datetime.date = Field(None, description=_("스케쥴의 날짜입니다"))
+    date_from: datetime.date = Field(
+        None, description=_("가져오려는 일정의 시작 일자"), q="date__gte"
+    )
+    date_to: datetime.date = Field(
+        None, description=_("가져오려는 일정의 끝 일자 (포함 X)"), q="date__lt"
+    )
 
     class Config(CamelCaseConfig, Schema.Config):
         pass
