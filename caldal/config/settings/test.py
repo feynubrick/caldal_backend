937cc5e6638e2b379d31c770feb5a7e3e6d97846
Seung-Yoon Oh <feynubrick@proton.me>
2024-11-03 18:47:11 +0900

    pytest 추가

---
+++ caldal/config/settings/test.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "10.0.2.2"]
+
+NINJA_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
+
+GOOGLE_OAUTH_REDIRECT_URI = "https://localhost:8000/auth/google"
+GOOGLE_OAUTH_CLIENT_ID = os.environ["GOOGLE_OAUTH_CLIENT_ID"]
