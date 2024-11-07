0db40ed95f285ccc185429e81a63df5ffc20ff29
Seung-Yoon Oh <feynubrick@proton.me>
2024-11-07 22:16:43 +0900

    business_service의 폴더 이름 규칙 정리

--- caldal/domain/account/business_services/__init__.py
+++ caldal/domain/account/business_services/__init__.py
@@ -1,3 +1,3 @@
 from .auth_token_service import AuthTokenService
-from .create_user_service import CreateUserService, CreateUserServiceInputSchema
+from .create_user import CreateUserService, CreateUserServiceInputSchema
 from .oauth_service import OAuthService
